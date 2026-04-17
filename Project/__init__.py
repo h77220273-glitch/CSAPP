090ac4bb8d7fe7945e3085c74873f9a695b395a7
h77220273-glitch <h77220273@gmail.com>
2026-04-17 17:06:46 +0200

    Update __init__.py

--- Project/__init__.py
+++ Project/__init__.py
@@ -4,33 +4,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config["SECRET_KEY"]='hello'
-login_manager=LoginManager(app)
-login_manager.login_view='login'
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-
-db = SQLAlchemy(app)
-Migrate(app,db)
-app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-
-#هندلة الايرورز
-@app.errorhandler(404)
-def error(er):
-    return "قريبا... شكرا لتجربتك متاح حاليا صفحة البوستات والبروفايل فقط", 404
-@app.errorhandler(502)
-def ero502(er):
-    return "حصل خطأ هصلحه..",502
-@app.errorhandler(500)
-def ero500(er):
-    return "سجل دخول",500
 
 def create_app():
+    global app
+    app = Flask(__name__)
+    app.config["SECRET_KEY"]='hello'
+    login_manager=LoginManager(app)
+    login_manager.login_view='login'
+    
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+    
+    db = SQLAlchemy(app)
+    Migrate(app,db)
+    app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+
+    #هندلة الايرورز
+    @app.errorhandler(404)
+    def error(er):
+        return "قريبا... شكرا لتجربتك متاح حاليا صفحة البوستات والبروفايل فقط", 404
+    @app.errorhandler(502)
+    def ero502(er):
+        return "حصل خطأ هصلحه..",502
+    @app.errorhandler(500)
+    def ero500(er):
+        return "سجل دخول",500
+
     from Project.Home import bp as homebp
     from Project.Auth import bp as authbp
     from Project.Profile import bp as profilebp
