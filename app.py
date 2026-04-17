d4f2da23e72864c6238aedb4869e20f1f857c315
h77220273-glitch <h77220273@gmail.com>
2026-04-17 17:08:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from Project import create_app,app
 
-app=app
+app = create_app()
 if __name__=="__main__":
-    create_app()
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
